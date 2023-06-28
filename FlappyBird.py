573fc6057560ba585635c89c02dea4469d3e5c78
robinsrarf <59559816+robinsrarf@users.noreply.github.com>
2023-06-28 16:19:45 +0530

    Update FlappyBird.py

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,11 +1,42 @@
 import pygame as pg
-W,H = 400, 500
-running  = True
+from sys import exit
+
+pg.init()
+W, H = 400, 500
+running = True
+color = pg.Color
 screen = pg.display.set_mode((W, H))
+pg.display.set_caption("FlappyBird")
+clock = pg.Clock()
 
 
+class Bird:
+    def __init__(self, pos, img):
+        self.pos = pos
+        self.vel = 1
+        self.a = 0
+        self.sprite = pg.image.load(img)
+        self.img_size = self.sprite.get_size()
 
-while (running):
+    def update(self):
+        if self.pos[1] < H:
+            self.vel += self.a
+            self.pos[1] += self.vel
+        screen.blit(self.sprite, (self.pos[0]-self.img_size[0]//2, self.pos[1]-self.img_size[1]//2))
+        #pg.draw.circle(screen, "red", self.pos, 5)
+
+
+player = Bird([W//2, H//2], "C:\\Users\\robin\\Desktop\\FlappyBird\\sprites\\bird.png")
+while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            running = False
+            pg.quit()
+            exit()
+        if event.type == pg.MOUSEBUTTONDOWN:
+            player.pos[1] += -30
+
+    screen.fill('black')
+    player.update()
+    
+    pg.display.update()
+    clock.tick(60)
